473260435daf160dddf0c00d6f9199849ee82e88
Iusti T <iustilaurentiu@yahoo.ro>
2023-06-17 21:56:22 +0100

    fixed initialising

---
+++ Block.py
@@ -0,0 +1,12 @@
+from Sprite import *
+
+class Block(Sprite):
+
+    def __init__(self, screen, x=0, y=0, colour=(240, 0, 0)):
+        Sprite.__init__(self, screen, x, y, colour)
+        
+    def draw():
+        pass
+
+    def move():
+        pass
