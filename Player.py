5de8011e76d5d098a7f2b6fe7cabe5472d7751d1
Iusti T <iustilaurentiu@yahoo.ro>
2023-06-20 01:10:59 +0100

    fixed drawing + position after movement

--- Player.py
+++ Player.py
@@ -2,19 +2,23 @@ from Sprite import *
 
 class Player(Sprite):
 
-    def __init__(self, screen, x=0, y=0, colour = (0, 240, 0)):
-        Sprite.__init__(self, screen, x, y, colour)
+    def __init__(self, game, x=0, y=0, colour = (0, 240, 0)):
+        super().__init__(game, x, y, colour)
+        # higher width, lower size (screen_height/width is size)
+        self.width = 16
         
+    # Handle player movement based on input.
     def move(self):
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    self._x = self._screen.get_width()*(0.25)
-                if event .key == pygame.K_s:
-                    self._x = self._screen.get_width()*(0.50)
-                if event .key == pygame.K_d:
-                    self._x = self._screen.get_width()*(0.75)
+                    self.x = self.screen.get_width()*(0.2)
+                if event.key == pygame.K_s:
+                    self.x = self.screen.get_width()*(0.5)
+                if event.key == pygame.K_d:
+                    self.x = self.screen.get_width()*(0.8)
 
+    # Draw the player on the screen.
     def draw(self):
-        pygame.draw.circle(self._screen, self._colour, (self._x, self._y), 20)                
+        pygame.draw.circle(self.screen, self.colour, (self.x, self.y), self.screen.get_height()/self.width)
