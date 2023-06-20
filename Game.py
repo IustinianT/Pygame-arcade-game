5817b53fbea4e5f9b976358b8e082c8749f75330
Iusti T <iustilaurentiu@yahoo.ro>
2023-06-21 00:31:10 +0100

    modularised code, simplified timers

--- Game.py
+++ Game.py
@@ -7,18 +7,25 @@ from threading import Timer
 class Game:
     background_colour = (0, 0, 0)
     (width, height) = (470, 650)
+
     blocks = []
     current_block_index = 0
+    spawn_block_interval = 0.4
+
+    timers = []
+    defeault_timers = []
 
     def __init__(self):
         self.create_window()
         self.create_player()
 
+    # Main loop of the program.
     def run(self):
         frames_per_second = 60
-        spawn_block_interval = 0.5
-        timer1 = Timer(spawn_block_interval, self.spawn_block)
-        timer1.start()
+
+        # create and start timers
+        self.initialise_timers()
+
         # main loop of the game
         running = True
         while running:
@@ -31,10 +38,8 @@ class Game:
             # draw all sprites on screen
             self.draw_sprites()
 
-            # spawn a block every x seconds
-            if not timer1.is_alive():
-                timer1 = Timer(spawn_block_interval, self.spawn_block)
-                timer1.start()
+            # update timers and reset once finished
+            self.update_timers()
             
             # if user presses X quit 'cleanly'
             for event in pygame.event.get():
@@ -71,10 +76,27 @@ class Game:
             if block.is_out_of_bounds():
                 self.blocks.remove(block)
 
+    # Create a block and add it to the 'blocks' list.
     def spawn_block(self):
         block_x = self.screen.get_width()*(random.randint(0,2)/3)
         self.blocks.append(Block(self, block_x, -self.height))
 
+    # Get the screen of the game.
     def get_screen(self):
         return self.screen
     
+    # Update the timers and remove if finished.
+    def update_timers(self):
+        for timer in self.timers:
+            if not timer.is_alive():
+                self.timers.remove(timer)
+                self.initialise_timers()
+
+    # Create timers with the recurring functions.
+    def initialise_timers(self):
+        timer1 = Timer(self.spawn_block_interval, self.spawn_block) # spawn block timer
+        if timer1 not in self.timers:
+            self.timers.append(timer1)
+        for timer in self.timers:
+            timer.start()
+            
